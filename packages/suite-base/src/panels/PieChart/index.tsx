2aa0d8c0c11fe436fd6132ef3a74e0887d5b0805
Yukihito Saito <yukky.saito@gmail.com>
2024-12-08 01:00:17 +0900

    copy from gauge
    Signed-off-by: Yukihito Saito <yukky.saito@gmail.com>

---
+++ packages/suite-base/src/panels/PieChart/index.tsx
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-License-Identifier: MPL-2.0
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { useMemo } from "react";
+
+import { useCrash } from "@lichtblick/hooks";
+import { PanelExtensionContext } from "@lichtblick/suite";
+import { CaptureErrorBoundary } from "@lichtblick/suite-base/components/CaptureErrorBoundary";
+import Panel from "@lichtblick/suite-base/components/Panel";
+import { PanelExtensionAdapter } from "@lichtblick/suite-base/components/PanelExtensionAdapter";
+import { createSyncRoot } from "@lichtblick/suite-base/panels/createSyncRoot";
+import ThemeProvider from "@lichtblick/suite-base/theme/ThemeProvider";
+import { SaveConfig } from "@lichtblick/suite-base/types/panels";
+
+import { PieChart } from "./PieChart";
+import { Config } from "./types";
+
+function initPanel(crash: ReturnType<typeof useCrash>, context: PanelExtensionContext) {
+  return createSyncRoot(
+    <CaptureErrorBoundary onError={crash}>
+      <ThemeProvider isDark>
+        <PieChart context={context} />
+      </ThemeProvider>
+    </CaptureErrorBoundary>,
+    context.panelElement,
+  );
+}
+
+type Props = {
+  config: Config;
+  saveConfig: SaveConfig<Config>;
+};
+
+function PieChartPanelAdapter(props: Props) {
+  const crash = useCrash();
+  const boundInitPanel = useMemo(() => initPanel.bind(undefined, crash), [crash]);
+
+  return (
+    <PanelExtensionAdapter
+      config={props.config}
+      saveConfig={props.saveConfig}
+      initPanel={boundInitPanel}
+      highestSupportedConfigVersion={1}
+    />
+  );
+}
+
+PieChartPanelAdapter.panelType = "PieChart";
+PieChartPanelAdapter.defaultConfig = {};
+
+export default Panel(PieChartPanelAdapter);
